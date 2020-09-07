2800b49d9832ab8d09651d5bf6bb1d73a54f5bd3
Алексей Рогатин <bamako99@mail.ru>
2020-09-07 20:46:26 +0300

    Код распределен по функциям. Добавлены комментарии к заданиям

--- src/ru/geekbrains/java_one/lesson_b/home/Main.java
+++ src/ru/geekbrains/java_one/lesson_b/home/Main.java
@@ -5,62 +5,95 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        int[] first = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0}; //Первый пункт
-        for (int i = 0; i < first.length; i++) {
-            if (first[i] == 0) {
-                first[i] = 1;
+        // Результат выполнения первого пункта
+        int[] first = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        System.out.println(Arrays.toString(changeNums(first)));
+
+        // Результат выполнения втогоро пункта
+        int[] second = new int[8];
+        System.out.println(Arrays.toString(fillArray(second)));
+
+        // Результат выполнения третьего пункта
+        int[] third = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println(Arrays.toString(doubleNumbers(third)));
+
+        // Результат выполнения четвертого пункта
+        int[][] fourth = new int[5][5];
+        System.out.println(Arrays.deepToString(fillDiagonal(fourth)).replace("],", "\n"));
+
+        // Результат выполнения пятого пункта
+        int[] fifth = new int[] {15, 25, 3, 19, -55, -100, 94, 66, 15, 0, 71};
+        System.out.println("Максимальное и минимальные значения равны: " + Arrays.toString(findMinAndMax(fifth))
+                .replace("[", "")
+                .replace("]", "")
+                .replace(",", " и"));
+
+        // Результат выполнения шестого пункта
+        int[] sixth = new int[] {2, 2, 2, 1, 2, 2, 10, 1};
+        System.out.println(checkBalance(sixth));
+
+        // Результат выполнения седьмого пункта
+        int[] seventh = new int[] {3, 5, 6, 1};
+        System.out.println(Arrays.toString(shift(seventh, -2)));
+    }
+
+//  1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+//  С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    private static int[] changeNums(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
             } else {
-                first[i] = 0;
+                array[i] = 0;
             }
         }
+        return array;
+    }
 
-        System.out.println(Arrays.toString(first));
-
-        int[] second = new int[8]; //Второй пункт
-        for (int i = 0, j = 0; i < second.length; i++, j+= 3) {
-            second[i] = j;
+//  2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+    private static int[] fillArray(int[] array) {
+        for (int i = 0, j = 0; i < array.length; i++, j+= 3) {
+            array[i] = j;
         }
+        return array;
+    }
 
-        System.out.println(Arrays.toString(second));
-
-        int[] third = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1}; //Третий пункт
-        for (int i = 0; i < third.length; i++) {
-            if (third[i] < 6) {
-                third[i] *= 2;
+//  3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+    private static int[] doubleNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
             }
         }
+        return array;
+    }
 
-        System.out.println(Arrays.toString(third));
-
-        int[][] fourth = new int[5][5]; //Четвертый пункт
-        for (int i = 0, j = 0; i < fourth.length; i++, j++) {
-            fourth[i][j] = 1;
+//  4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+//  и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
+    private static int[][] fillDiagonal(int[][] array) {
+        for (int i = 0, j = 0; i < array.length; i++, j++) {
+            array[i][j] = 1;
         }
+        return array;
+    }
 
-        System.out.println(Arrays.deepToString(fourth).replace("],", "\n"));
-
-        int[] fifth = new int[] {15, 25, 3, 19, -55, -100, 94, 66, 15, 0, 71}; //Пятый пункт
-        int max = fifth[0];
-        int min = fifth[0];
-        for (int value : fifth) {
+//  5. Задать одномерный массив и найти в нем минимальный и максимальный элементы
+    private static int[] findMinAndMax(int[] array) {
+        int max = array[0];
+        int min = array[0];
+        for (int value : array) {
             if (value > max) {
                 max = value;
             } else if (min > value) {
                 min = value;
             }
         }
-
-        System.out.println("Max = " + max + ", min = " + min);
-
-        //Проверки результатов выполнения функций для пунктов 6 и 7
-        int[] arr = new int[] {2, 2, 2, 1, 2, 2, 10, 1};
-        System.out.println(checkBalance(arr));
-
-        int[] arr2 = new int[] {3, 5, 6, 1};
-        System.out.println(Arrays.toString(shift(arr2, -2)));
+        return new int[] {max, min};
     }
 
-    private static boolean checkBalance(int[] array) { //Шестой пункт
+//  6. Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true,
+//  если в массиве есть место, в котором сумма левой и правой части массива равны.
+    private static boolean checkBalance(int[] array) {
         int firstPartSum = 0;
         int secondPartSum = 0;
         for (int i = 0; i < array.length; i++) {
@@ -76,8 +109,9 @@ public class Main {
         return false;
     }
 
-    private static int[] shift(int[] array, int n) { //Седьмой пункт
-
+//  7. Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным),
+//  при этом метод должен сместить все элементымассива на n позиций.
+    private static int[] shift(int[] array, int n) {
         if (n > 0) { //Если передано положительное число, производим сдвиг вправо
             int lastElement;
 
