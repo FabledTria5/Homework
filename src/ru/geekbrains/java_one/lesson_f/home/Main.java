6239594bea200eb7bf535f6f77571cbb2ed9ffd0
Alex <bamako99@mail.ru>
2020-09-22 15:36:55 +0300

    Сделан весь функционал

--- src/ru/geekbrains/java_one/lesson_f/home/Main.java
+++ src/ru/geekbrains/java_one/lesson_f/home/Main.java
@@ -1,20 +1,35 @@
 package ru.geekbrains.java_one.lesson_f.home;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    private static final String[] dictionary = {"Кот ", "Собака ", "Дрога ", "Дом ", "Небо ", " Солнце ", "Самолет ", "Море ", "Природа ", "Программа ", "Музей "};
+    private static final String[] dictionary = {"Кот ", "Собака ", "Дрога ", "Дом ", "Небо ", " Солнце ", "Самолет ", "\n", "Море ", "Природа ", "Программа ", "Музей "};
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static Scanner fileScanner;
 
     public static void main(String[] args) {
         createFiles(2, 200);
         unitFiles();
+
+        if (isWordInFile("file1.txt", "Небо ")) {
+            System.out.println("Искомое слово присутствует в файле");
+        } else {
+            System.out.println("Файл не содержит искомого слова");
+        }
+
+        if (isWordInDirectory(".", "Солнце")) {
+            System.out.println("Искомое слово присутствует в каталоге");
+        } else {
+            System.out.println("Файлы в каталоге не содержат искомого слова");
+        }
+
+
     }
 
+    // Создать 2 текстовых файла, примерно по 50-100 символов в каждом
     private static void createFiles(int quantity, int symbols) {
         for (int i = 0; i < quantity; i++) {
             try {
@@ -36,6 +51,7 @@ public class Main {
         }
     }
 
+    // Написать метод, «склеивающий» эти файлы, то есть вначале идет текст из первого файла, потом текст из второго.
     private static void unitFiles() {
         System.out.print("Сколько файлов вы хотите объединить? ");
         int num = scanner.nextInt();
@@ -52,17 +68,52 @@ public class Main {
             try {
                 StringBuilder sb = new StringBuilder();
                 PrintStream ps = new PrintStream(new FileOutputStream("file" + (files.length + 1) + ".txt", true));
-                Scanner fileScanner = new Scanner(new FileInputStream(file));
+                fileScanner = new Scanner(new FileInputStream(file));
 
                 while (fileScanner.hasNextLine()) {
-                    sb.append(fileScanner.nextLine());
+                    ps.print(fileScanner.nextLine() + "\n");
                 }
 
-                ps.println(sb);
+                fileScanner.close();
                 ps.close();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    // Написать метод, который проверяет присутствует ли указанное пользователем слово в файле
+    private static boolean isWordInFile(String fileName, String word) {
+
+        try {
+
+            fileScanner = new Scanner(new FileInputStream(fileName));
+
+            while (fileScanner.hasNext()) {
+                if (fileScanner.nextLine().toLowerCase().contains(word.toLowerCase())) return true;
+            }
+
+            fileScanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    // Написать метод, проверяющий, есть ли указанное слово в папке
+    private static boolean isWordInDirectory(String directoryName, String word) {
+        File directory = new File(directoryName);
+        File[] filesInDir = directory.listFiles();
+
+        if (filesInDir == null) return false;
+
+        for (File file : filesInDir) {
+            if (file.isFile() && isWordInFile(file.getName(), word)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
